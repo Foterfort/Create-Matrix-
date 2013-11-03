7306fa2a3b61f6b2354ae636cb06a41916846534
Biseuv Olzhas <dosymep@gmail.com>
2013-11-03 21:04:15 +0700

    first commit

---
+++ func/SortWords.cpp
@@ -0,0 +1,37 @@
+﻿#include "stdafx.h"
+#include "SortWords.h"
+
+
+SortWords::SortWords() {
+}
+
+SortWords::~SortWords() {
+}
+
+bool SortWords::create‏(string path) {
+
+	ifstream myfile (path);
+
+	if (myfile.is_open()) {
+		while (!myfile.eof()) {
+			getline(myfile, _line);
+			words.push_back(_line);
+		}
+		myfile.close();
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool SortWords::createListWords(int sizeX, int sizeY) {
+	return 0;
+}
+
+char SortWords::createArrayHV() {
+	return 0;
+}
+
+void SortWords::getStructWords() {
+}
---
+++ func/SortWords.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class SortWords {
+public:
+	SortWords();
+	virtual ~SortWords();
+
+	bool create‏(string path);
+	bool createListWords(int sizeX, int sizeY);
+	char createArrayHV();
+	void getStructWords();
+
+private:
+	struct wordsStruct {
+		string word; 
+		int xStart, yStart;
+		int xEnd, yEnd;
+	} worker;
+
+	int sizeX;
+	int sizeY;
+
+	string _line;
+
+	vector<string> words;
+	vector<string> wordsSelection;
+	list<string> listWords;
+};
+
